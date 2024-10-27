984c885c0f957a4fea5c5226d1800679ca14af14
Venkatesh Reddy <venkateshreddy.k@techbulls.co.in>
2024-10-27 18:03:00 +0530

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
- 
+import dynamic from 'next/dynamic';
+
 export default function RootLayout({
   children,
 }: {
@@ -8,7 +9,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+           <body className={`${inter.className} antialiased`}>{children}</body>
+ 
     </html>
   );
 }
